b871c95450312deaa7df3989bedf305506db1a40
gabenasci <gabenascimento1@gmail.com>
2020-10-27 14:22:38 -0300

    criação da classe Funcionario

---
+++ entidade/funcionario.py
@@ -0,0 +1,11 @@
+from entidade.pessoa import Pessoa
+from entidade.servico import Servico
+
+class Funcionario(Pessoa):
+    def __init__(self, nome: str, data_nascimento: str, telefone: int, data_contratacao: str, servico: Servico):
+        super().__init__(nome, data_nascimento, telefone)
+        if isinstance(data_contratacao, str):
+            self.__data_contratracao = data_contratacao
+        if isinstance(servico, Servico):
+            self.__servico = servico
+
